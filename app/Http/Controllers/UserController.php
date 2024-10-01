8cd7d4770e8ebffa331fe652a44ea10251e5ab51
Eriza Tri Sativa <eriza.sativa66@gmail.com>
2024-10-01 19:50:46 +0700

    pesan commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function profile($nama = '', $kelas ='', $npm = ''){
+        $data = [
+            'nama' => $nama,
+            'kelas' => $kelas,
+            'npm' => $npm
+        ];
+
+        return view('profile', $data);
+
+    }
+
+    public function create(){
+        return view('create_user');
+       }
+       
+}
