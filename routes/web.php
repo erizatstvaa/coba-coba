9af5eaf610ea4b4ad28bbefb2f6e9c526d4a7e67
Eriza Tri Sativa <eriza.sativa66@gmail.com>
2024-10-05 03:34:15 +0700

    pesan commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', [ProfileController::class, 'profile']);
+Route::get('/profile/{nama}',[ProfileController::class, 'profile']);
+Route::get('/profile/{nama}/{kelas}/{npm}',[ProfileController::class, 'profile']);
+Route::get('/user/profile', [UserController::class,'profile']);
+Route::get('/user/create', [UserController::class,'create']);
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+Route::get('/user/list', [UserController::class, 'index'])->name('user.index');
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
